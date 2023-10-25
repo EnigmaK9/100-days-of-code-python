6746ccc79dc3aef16be52b32fd5f827cb57e2ebc
Ignacio Padilla <enigmak9@protonmail.com>
2023-10-25 00:03:43 -0600

    spacebar addded

--- 01-day-printing-start/proyect/band-name-generator.py
+++ 01-day-printing-start/proyect/band-name-generator.py
@@ -2,10 +2,10 @@
 print("Greetings mortal, are you ready to die?")
 
 # 2. Ask the user for the city that they grew up in.
-city = input("City where you grew up: ")
+city = input("City where you grew up: \n")
 
 # 3. Ask the user for the name of a pet.
-pet_name = input("Name of your pet: ")
+pet_name = input("Name of your pet: \n")
 
 # 4. Combine the name of their city and pet and show them their band name.
 band_name = city + " " + pet_name
