fb03db82978d90d8c030c1bfc4356437be83823e
Ignacio Padilla <enigmak9@protonmail.com>
2023-11-22 03:37:07 -0600

    final version of life-in-weeks pushed

---
+++ 02-data-types/life-in-weeks.py
@@ -0,0 +1,8 @@
+#4000 thousands in our lifetiem
+# relatively easy to understand
+# how little time we have on earth
+age = input("type your age: ")
+
+years = 90 - int(age)
+weeks = years * 52
+print(f"You have {weeks} weeks left.")
