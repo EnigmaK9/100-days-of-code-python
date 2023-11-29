06ee372ba1b8902039d286d8798a32704c365a79
Ignacio Padilla <enigmak9@protonmail.com>
2023-11-29 14:04:43 -0600

    added odd or even exercise

---
+++ 03-control-flow/odd-or-even.py
@@ -0,0 +1,9 @@
+# Which number do you want to check?
+print("Type a number. We'll find if it's even or odd :)")
+number = int(input())
+# Don't change the code above
+# Write your code below this line
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
