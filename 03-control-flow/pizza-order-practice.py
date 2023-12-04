dee22631b28a759c2e04698f259a9c47ad205b0a
Ignacio Padilla <enigmak9@protonmail.com>
2023-12-03 20:10:49 -0600

    added pizza order

--- 03-control-flow/pizza-order-practice.py
+++ 03-control-flow/pizza-order-practice.py
@@ -1,7 +1,7 @@
 print("Thank you for choosing python pizza deliveries!")
-size = input()
-add_pepperoni = input()
-extra_cheese = input()
+size = input("size: S or M ")
+add_pepperoni = input("pepperoni: S or M ")
+extra_cheese = input("cheese: Y or N ")
 bill = 0
 if size == "S":
     bill += 15
@@ -9,3 +9,11 @@ elif size == "M":
     bill += 20
 else:
     bill += 25
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}.")
