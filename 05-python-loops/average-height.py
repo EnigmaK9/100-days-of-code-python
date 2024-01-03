67f86a758d19fbb86ef74ac876a792b501d0f1c9
Ignacio Padilla <enigmak9@protonmail.com>
2024-01-02 23:59:17 -0600

    updated

---
+++ 05-python-loops/average-height.py
@@ -0,0 +1,9 @@
+# students height in centimeters
+# list
+# write a for loop
+# len method, sun method
+# ignore those, pretend they didn't exist
+# for loop
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
