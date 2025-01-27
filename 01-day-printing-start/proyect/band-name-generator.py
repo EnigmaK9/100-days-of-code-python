b062c67ee282aebbaeb040c39173f99798b1a184
Enigma ♘ <enigmak9@protonmail.com>
2025-01-27 02:14:16 -0600

    added other chapters

---
+++ 01-day-printing-start/proyect/band-name-generator.py
@@ -0,0 +1,14 @@
+# 1. Create a greeting for your program.
+print("Greetings mortal, are you ready to die?")
+
+# 2. Ask the user for the city that they grew up in.
+city = input("City where you grew up: \n")
+
+# 3. Ask the user for the name of a pet.
+pet_name = input("Name of your pet: \n")
+
+# 4. Combine the name of their city and pet and show them their band name.
+band_name = city + " " + pet_name
+
+# 5. Make sure the input cursor shows on a new line:
+print("\nYour band name is:", band_name)
---
+++ 05-python-loops/for-loops-and-range.py
@@ -0,0 +1,6 @@
+# using for loops for the range function
+for number in range(1, 11, 3):
+    # all the numbers, another comma and specify how large you want the step to
+    # be
+    print(number)
+
